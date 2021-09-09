9cf8fd803a34133ef459f71e4275122133977545
tygrski <77369211+tygrski@users.noreply.github.com>
2021-09-09 15:01:06 -0500

    data bugs

---
+++ assets/js/single.js
@@ -0,0 +1,47 @@
+var getReposIssues = function(repo) {
+  console.log(repo);
+  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+  console.log("test")
+  fetch(apiUrl).then(function(response) {
+  if (response.ok) {
+    response.json().then(function(data) {
+      // pass response to DOM function
+      displayIssues(data);
+    });
+    }
+    else {
+      alert("there was a problem with your request");
+    }
+    });
+  };
+
+getReposIssues("facebook/react");
+
+var displayIssues = function(issues) {
+  for (var i = 0; i < issues.length; i++) {
+    // create a link element to take users to the issue on github
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+  }
+  // create span to hold issue title
+  var titleEl = document.createElement("span");
+  titleEl.textContent = issues[i].title;
+
+  // append to container
+  issueEl.appendChild(titleEl);
+
+  // create a type element
+  var typeEl = document.createElement("span");
+
+  // check if issue is an actual issue or a pull request
+  if (issues[i].pull_request) {
+    typeEl.textContent = "(Pull request)";
+  } else {
+    typeEl.textContent = "(Issue)";
+  }
+
+  // append to container
+issueEl.appendChild(typeEl);
+};
