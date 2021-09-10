9bc2af72010513cdf770ea8169da3d75c9eab045
tygrski <77369211+tygrski@users.noreply.github.com>
2021-09-10 18:00:00 -0500

    bug

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,13 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+function getRepoName() {
+var queryString = document.location.search;
+var repoName = queryString.split("=")[1];
+console.log(repoName);
+};
+getRepoName();
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -81,4 +88,3 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
